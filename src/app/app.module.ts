161ee5aa496b3c88f5d826e8057702e27d49fcec
kristibyrnes <kristibyrnes@icloud.com>
2018-05-04 11:33:04 -0400

    barcodescanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { CameraPreview, CameraPreviewOptions } from '@ionic-native/camera-preview';
+import { CameraPreview } from '@ionic-native/camera-preview';
 
 
 import { MyApp } from './app.component';
@@ -34,8 +34,8 @@ import { ScannerPage } from '../pages/scannerPage/scannerPage';
     StatusBar,
     SplashScreen,
     BarcodeScanner,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    CameraPreview
+    CameraPreview,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
--- src/pages/scannerPage/scannerPage.ts
+++ src/pages/scannerPage/scannerPage.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+// import { CameraPreview } from '@ionic-native/camera-preview';
+
 
 @Component({
   selector: 'page-scannerPage',
@@ -8,14 +10,35 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 })
 export class ScannerPage {
 
-  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
-    this.barcodeScanner.scan().then(barcodeData => {
-      console.log('Barcode data', barcodeData);
-     }).catch(err => {
-         console.log('Error', err);
-     });
-   }
-    refresh(){
-      window['location'].reload();
+  // constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
+    
+  //   this.barcodeScanner.scan().then(barcodeData => {
+  //     console.log('Barcode data', barcodeData);
+  //    }).catch(err => {
+  //        console.log('Error', err);
+  //    });
+  //  }
+
+   scanData : {};
+   options : BarcodeScannerOptions;
+   constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
+  }    
+
+  scan(){
+    console.log("what it is?");
+    this.options = {
+        prompt : "Scan your barcode "
     }
+    this.barcodeScanner.scan(this.options).then((barcodeData) => {
+
+        console.log(barcodeData);
+        this.scanData = barcodeData;
+    }, (err) => {
+        console.log("Error occured : " + err);
+    });         
+
+  }
+    // refresh(){
+    //   window['location'].reload();
+    // }
 }
